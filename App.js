5a5aa278aca703e20b4cfd2d974cf11b04473d7d
hassanz93 <hassanzahra911@gmail.com>
2022-09-27 17:23:50 +0300

    master branch

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+
+  return (
+   <View>
+    <Text>Music Player</Text>
+   </View>
+  );
+};
+
+export default App;
